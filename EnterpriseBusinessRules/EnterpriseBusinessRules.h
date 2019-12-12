81667bf21043003554d38003c6776632e57be161
Milos Dimic <dimic.milos@icloud.com>
2019-12-12 10:38:19 +0100

    Initial Commit

---
+++ EnterpriseBusinessRules/EnterpriseBusinessRules.h
@@ -0,0 +1,19 @@
+//
+//  EnterpriseBusinessRules.h
+//  EnterpriseBusinessRules
+//
+//  Created by Dimic Milos on 12/12/19.
+//  Copyright © 2019 Dimic Milos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EnterpriseBusinessRules.
+FOUNDATION_EXPORT double EnterpriseBusinessRulesVersionNumber;
+
+//! Project version string for EnterpriseBusinessRules.
+FOUNDATION_EXPORT const unsigned char EnterpriseBusinessRulesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EnterpriseBusinessRules/PublicHeader.h>
+
+
